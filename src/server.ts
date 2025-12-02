d9ef9c692b4a9c36ef4923528be5c995827e8117
Dipta-b <diptabanik0@gmail.com>
2025-12-02 16:07:19 +0600

    table created

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,26 @@ const port = 5000
 //parser
 app.use(express.json())
 
+//DB
 const pool = new Pool({
     connectionString: `postgresql://neondb_owner:npg_rkW69ETmibYh@ep-still-truth-ah82ery0-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
 });
+//primary key diye shob ber kroe niye asha jay
+const initDB = async () => {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,  
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updatd_at TIMESTAMP DEFAULT NOW()
+        `)
+}
+
+initDB();
 
 //form data pete 
 // app.use(express.urlencoded())
