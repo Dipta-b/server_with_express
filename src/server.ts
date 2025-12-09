6fc7840ae8d260774acdcc52dc61ad04bad34542
Dipta-b <diptabanik0@gmail.com>
2025-12-09 20:31:45 +0600

    table and todos created

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 const app = express()
 const port = 5000
 //parser
@@ -7,22 +10,37 @@ app.use(express.json())
 
 //DB
 const pool = new Pool({
-    connectionString: `postgresql://neondb_owner:npg_rkW69ETmibYh@ep-still-truth-ah82ery0-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+    connectionString: `${process.env.CONNECTION_STR}`
 });
 //primary key diye shob ber kroe niye asha jay
 const initDB = async () => {
     await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,  
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL UNIQUE,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
+            id SERIAL PRIMARY KEY,  
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            age INT,
+            phone VARCHAR(15),
+            address TEXT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        );
+    `);
+    console.log("Users table created successfully 🚀");
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+         title VARCHAR(200) NOT NULL,
+         description TEXT,
+         completed BOOLEAN DEFAULT FALSE,
+        due_date DATE,
         created_at TIMESTAMP DEFAULT NOW(),
-        updatd_at TIMESTAMP DEFAULT NOW()
-        `)
-}
+        updated_at TIMESTAMP DEFAULT NOW()
+        ); `);
+};
+
 
 initDB();
 
